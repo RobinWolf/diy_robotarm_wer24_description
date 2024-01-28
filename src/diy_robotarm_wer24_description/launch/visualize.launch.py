211e43914174472a707744c73c5962298bc7e033
Robin Wolf <woro1021@h-ka.de>
2024-01-28 17:11:42 +0100

    troubleshooting on yaml files and xacros finished

--- src/diy_robotarm_wer24_description/launch/visualize.launch.py
+++ src/diy_robotarm_wer24_description/launch/visualize.launch.py
@@ -6,78 +6,24 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    declared_arguments = []
+    description_package = "diy_robotarm_wer24_description"
 
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "safety_limits",
-            default_value="true",
-            description="Enables the safety limits controller if true.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "safety_pos_margin",
-            default_value="0.15",
-            description="The margin to lower and upper limits in the safety controller.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "safety_k_position",
-            default_value="20",
-            description="k-position factor in the safety controller.",
-        )
-    )
-    # General arguments
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_package",
-            default_value="ur_description",
-            description="Description package with robot URDF/XACRO files. Usually the argument \
-        is not set, it enables use of a custom description.",
-        )
-    )
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            "description_file",
-            default_value="diy_robotarm_main.urdf.xacro",
-            description="URDF/XACRO description file with the robot.",
-        )
-    )
+    declared_arguments = []
     declared_arguments.append(
         DeclareLaunchArgument(
             "tf_prefix",
             default_value='""',
-            description="Prefix of the joint names, useful for \
-        multi-robot setup. If changed than also joint names in the controllers' configuration \
-        have to be updated.",
+            description="Prefix for the links and joints in the robot cell",
         )
     )
 
-    # Initialize Arguments
-    safety_limits = LaunchConfiguration("safety_limits")
-    safety_pos_margin = LaunchConfiguration("safety_pos_margin")
-    safety_k_position = LaunchConfiguration("safety_k_position")
-    # General arguments
-    description_package = LaunchConfiguration("description_package")
-    description_file = LaunchConfiguration("description_file")
     tf_prefix = LaunchConfiguration("tf_prefix")
 
     robot_description_content = Command(
         [
             PathJoinSubstitution([FindExecutable(name="xacro")]),
             " ",
-            PathJoinSubstitution([FindPackageShare(description_package), "urdf", description_file]),
-            " ",
-            "safety_limits:=",
-            safety_limits,
-            " ",
-            "safety_pos_margin:=",
-            safety_pos_margin,
-            " ",
-            "safety_k_position:=",
-            safety_k_position,
+            PathJoinSubstitution([FindPackageShare(description_package), "urdf", "diy_robotarm_main.urdf.xacro"]),
             " ",
             "tf_prefix:=",
             tf_prefix,
@@ -85,14 +31,9 @@ def generate_launch_description():
     )
     robot_description = {"robot_description": robot_description_content}
 
-    #rviz_config_file = PathJoinSubstitution(
-     #   [FindPackageShare(description_package), "rviz", "view_robot.rviz"]
-   # )
+    #rviz_config_file = PathJoinSubstitution([FindPackageShare(description_package), "rviz", "rviz_config.rviz"]) # define path to rviz-config file
 
-   # joint_state_publisher_node = Node(
-   #     package="joint_state_publisher_gui",
-   #     executable="joint_state_publisher_gui",
-   # )
+
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -104,13 +45,12 @@ def generate_launch_description():
         executable="rviz2",
         name="rviz2",
         output="log",
-        #arguments=["-d", rviz_config_file],
+        #arguments=["-d", rviz_config_file]
     )
 
     nodes_to_start = [
-       # joint_state_publisher_node,
         robot_state_publisher_node,
-        rviz_node,
+        rviz_node
     ]
 
     return LaunchDescription(declared_arguments + nodes_to_start)
