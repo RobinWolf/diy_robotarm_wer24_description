3e4365db398d6042d1688e2c2db64bde5443c8d0
Robin Wolf <woro1021@h-ka.de>
2024-01-28 20:59:35 +0100

    modify run.sh and Dockerfile for deployment, cloning package from github

--- src/build/diy_robotarm_wer24_description/CMakeFiles/diy_robotarm_wer24_description_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/diy_robotarm_wer24_description_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/diy_robotarm_wer24_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
